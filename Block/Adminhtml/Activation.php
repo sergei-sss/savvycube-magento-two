459064758a23c29b62c70a974a8fa952eb5e77b5
Vitali Tsimoshka <v.tsimoshka@gmail.com>
2021-05-25 13:37:42 +0300

    import version 2.0.7

--- Block/Adminhtml/Activation.php
+++ Block/Adminhtml/Activation.php
@@ -8,11 +8,24 @@ use Magento\Framework\Data\Form\Element\AbstractElement;
 
 class Activation extends Field
 {
+    protected $backendUrl;
 
+    public function __construct(
+        \Magento\Backend\Model\Url $backendUrlManager,
+        \Magento\Backend\Block\Template\Context $context,
+        array $data = []
+    ) {
+
+        $this->backendUrl = $backendUrlManager;
+        parent::__construct($context, $data);
+
+    }
     protected function _getElementHtml(AbstractElement $element)
     {
+
         $element->setValue(__('Connect'));
-        $element->setOnclick("setLocation('".$this->getUrl('savvyadmin/index/index')."')");
+        $element->setOnclick("setLocation('" . $this->backendUrl->getUrl('savvyadmin/index/index') . "')");
         return parent::_getElementHtml($element);
+
     }
 }
